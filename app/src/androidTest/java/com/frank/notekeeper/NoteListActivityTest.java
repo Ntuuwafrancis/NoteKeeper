5064ace9e3a9c77ac54ee6e445b569c0d6f4b060
Ntuuwafrancis <51553342+Ntuuwafrancis@users.noreply.github.com>
2020-07-13 18:36:35 +0300

    NoteKeeper git commit 06(Tools & Testing course done)

---
+++ app/src/androidTest/java/com/frank/notekeeper/NoteListActivityTest.java
@@ -0,0 +1,133 @@
+package com.frank.notekeeper;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class NoteListActivityTest {
+
+    @Rule
+    public ActivityTestRule<NoteListActivity> mActivityTestRule = new ActivityTestRule<>(NoteListActivity.class);
+
+    @Test
+    public void noteListActivityTest() {
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.fab),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        floatingActionButton.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(700);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.text_note_title),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.coordinatorlayout.widget.CoordinatorLayout")),
+                                        1),
+                                1),
+                        isDisplayed()));
+        appCompatEditText.perform(click());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.text_note_title),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.coordinatorlayout.widget.CoordinatorLayout")),
+                                        1),
+                                1),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("Title"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.text_note_text),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.coordinatorlayout.widget.CoordinatorLayout")),
+                                        1),
+                                2),
+                        isDisplayed()));
+        appCompatEditText3.perform(replaceText("Body"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText4 = onView(
+                allOf(withId(R.id.text_note_text), withText("Body"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.coordinatorlayout.widget.CoordinatorLayout")),
+                                        1),
+                                2),
+                        isDisplayed()));
+        appCompatEditText4.perform(click());
+
+        ViewInteraction appCompatEditText5 = onView(
+                allOf(withId(R.id.text_note_text), withText("Body"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.coordinatorlayout.widget.CoordinatorLayout")),
+                                        1),
+                                2),
+                        isDisplayed()));
+        appCompatEditText5.perform(click());
+
+        pressBack();
+
+        pressBack();
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
